41de22e271bed0465d4e42c6c954405ffaf55c89
mohammed alqerem <mahammedalqerem422@gmail.com>
2026-01-17 09:39:44 +0200

    finish the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import Navbar from './navbar/Navbar'
+import Hero from './heroSection/Hero'
 
 export default function App() {
   return (
     <>
 
        <Navbar/>
+       <Hero/>
 
          
       
